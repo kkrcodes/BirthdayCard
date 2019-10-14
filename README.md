5b2945cec03297d215e3ffd7c92da51c11121ec2
kkrcodes <kkr.webdev@gmail.com>
2019-10-14 19:28:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BirthdayCard
+ Birthday Card App for Udacity's Android Basics: User Interface course
---
+++ app/src/main/java/com/example/android/birthdaycard/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.birthdaycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * This app displays a birthday card
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
